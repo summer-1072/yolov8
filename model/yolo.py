448fc78d094c9c4705cbbb47807c72ffb3f8df8a
wangkai <kaiwang.job@outlook.com>
2023-05-15 16:44:37 +0800

    update

--- model/yolo.py
+++ model/yolo.py
@@ -76,30 +76,27 @@ class YOLOBI(nn.Module):
         # BackBone
         self.p1 = eval(network[0])
         self.p2 = eval(network[1])
-        self.c3f_1 = eval(network[2])
+        self.c2f_1 = eval(network[2])
         self.p3 = eval(network[3])
-        self.c3f_2 = eval(network[4])
+        self.c2f_2 = eval(network[4])
         self.p4 = eval(network[5])
-        self.c3f_3 = eval(network[6])
+        self.c2f_3 = eval(network[6])
         self.p5 = eval(network[7])
-        self.c3f_4 = eval(network[8])
+        self.c2f_4 = eval(network[8])
         self.p6 = eval(network[9])
-        self.c3f_5 = eval(network[10])
+        self.c2f_5 = eval(network[10])
         self.sppf = eval(network[11])
 
         # Neck
-        self.p7 = eval(network[12])
-        self.c3f_6 = eval(network[13])
-        self.p8 = eval(network[14])
-        self.c3f_7 = eval(network[15])
-        self.p9 = eval(network[16])
-        self.c3f_8 = eval(network[17])
-        self.p10 = eval(network[18])
-        self.c3f_9 = eval(network[19])
-        self.p11 = eval(network[20])
-        self.c3f_10 = eval(network[21])
-        self.p12 = eval(network[22])
-        self.c3f_11 = eval(network[23])
+        self.c2f_6 = eval(network[12])
+        self.c2f_7 = eval(network[13])
+        self.c2f_8 = eval(network[14])
+        self.p7 = eval(network[15])
+        self.c2f_9 = eval(network[16])
+        self.p8 = eval(network[17])
+        self.c2f_10 = eval(network[18])
+        self.p9 = eval(network[19])
+        self.c2f_11 = eval(network[20])
 
         self.upsample1 = nn.Upsample(scale_factor=2, mode='nearest')
         self.upsample2 = nn.Upsample(scale_factor=2, mode='nearest')
@@ -121,37 +118,40 @@ class YOLOBI(nn.Module):
         # BackBone
         y1 = self.p1(x)
         y2 = self.p2(y1)
-        y3 = self.c3f_1(y2)
+        y3 = self.c2f_1(y2)
         y4 = self.p3(y3)
-        y5 = self.c3f_2(y4)
+        y5 = self.c2f_2(y4)
         y6 = self.p4(y5)
-        y7 = self.c3f_3(y6)
+        y7 = self.c2f_3(y6)
         y8 = self.p5(y7)
-        y9 = self.c3f_4(y8)
+        y9 = self.c2f_4(y8)
         y10 = self.p6(y9)
-        y11 = self.c3f_5(y10)
+        y11 = self.c2f_5(y10)
         y12 = self.sppf(y11)
 
         # Neck
         # FPN UP
-        y13 = self.p7(y12)
-        y14 = self.c3f_6(torch.cat([self.upsample1(y13), y9], 1))
+        y13 = torch.cat([self.upsample1(y12), y9], 1)
+        y14 = self.c2f_6(y13)
 
-        y15 = self.p8(y14)
-        y16 = self.c3f_7(torch.cat([self.upsample2(y15), y7], 1))
+        y15 = torch.cat([self.upsample2(y14), y7], 1)
+        y16 = self.c2f_7(y15)
 
-        y17 = self.p9(y16)
-        y18 = self.c3f_8(torch.cat([self.upsample3(y17), y5], 1))
+        y17 = torch.cat([self.upsample3(y16), y5], 1)
+        y18 = self.c2f_8(y17)
 
         # FPN DOWN
-        y19 = self.c3f_9(torch.cat([self.p10(y18), y17, self.downsample1(y5)], 1))
+        y19 = torch.cat([self.p7(y18), y16, self.downsample1(y5)], 1)
+        y20 = self.c2f_9(y19)
 
-        y20 = self.c3f_10(torch.cat([self.p11(y19), y15 , self.downsample1(y7)], 1))
+        y21 = torch.cat([self.p8(y20), y14, self.downsample2(y7)], 1)
+        y22 = self.c2f_10(y21)
 
-        y21 = self.c3f_11(torch.cat([self.p12(y20), y13], 1))
+        y23 = torch.cat([self.p9(y22), y12], 1)
+        y24 = self.c2f_11(y23)
 
         # Head
-        y = [y18, y19, y20, y21]
+        y = [y18, y20, y22, y24]
         y = [torch.cat((self.conv1[i](y[i]), self.conv2[i](y[i])), 1) for i in range(len(y))]
 
         return self.anchor(y)
